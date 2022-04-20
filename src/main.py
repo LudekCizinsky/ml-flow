90388ca926d4921fffc831d2526d0b29cebd5b02
LudekCizinsky <ludekcizinsky11@gmail.com>
2022-04-20 13:22:19 +0200

    I am able to execute template and get some results.

---
+++ src/main.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import mlflow
+import os
+path = os.path.dirname(os.path.abspath(__file__))
+
+from azureml.core import Workspace
+ws = Workspace.from_config()
+mlflow.set_tracking_uri(ws.get_mlflow_tracking_uri())
+
+## NOTE: Optionally, you can use the public tracking server.  Do not use it for data you cannot afford to lose. See note in assignment text. If you leave this line as a comment, mlflow will save the runs to your local filesystem.
+
+# mlflow.set_tracking_uri("http://training.itu.dk:5000/")
+
+mlflow.set_experiment("luci - First try: Linear regression with polynomial features")
+
+# Import some of the sklearn modules you are likely to use.
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.svm import SVR
+from sklearn.model_selection import TimeSeriesSplit
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+
+# Start a run
+with mlflow.start_run(run_name="Test"):
+
+    df = pd.read_json(path + "/data/raw/dataset.json", orient="split")
+
+    df = df.dropna()
+
+    pipeline = Pipeline([('scaler', StandardScaler()), ('LR', LinearRegression())])
+
+    metrics = [
+        ("MAE", mean_absolute_error, []),
+    ]
+
+    X = df[["Speed"]]
+    y = df["Total"]
+
+    number_of_splits = 5
+ 
+    for train, test in TimeSeriesSplit(number_of_splits).split(X,y):
+        pipeline.fit(X.iloc[train],y.iloc[train])
+        predictions = pipeline.predict(X.iloc[test])
+        truth = y.iloc[test]
+
+        from matplotlib import pyplot as plt 
+        plt.plot(truth.index, truth.values, label="Truth")
+        plt.plot(truth.index, predictions, label="Predictions")
+        #plt.show()
+        
+        # Calculate and save the metrics for this fold
+        for name, func, scores in metrics:
+            score = func(truth, predictions)
+            scores.append(score)
+    
+    # Log a summary of the metrics
+    for name, _, scores in metrics:
+        mean_score = sum(scores)/number_of_splits
+        mlflow.log_metric(f"mean_{name}", mean_score)
+
